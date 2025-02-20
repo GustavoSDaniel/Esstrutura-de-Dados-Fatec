b09b75ef21ef1e5a086131d09ef5fb5e14bfec9c
GustavoSDaniel <gustavosdaniel@hotmail.com>
2025-02-19 22:38:21 -0300

    Aula 2

--- 02_listas.py
+++ 02_listas.py
@@ -2,7 +2,7 @@
 
 #Lista de strings
 
-frutas = ["maça", "morango", "laranja", "uva", "manga" "goiaba"]
+frutas = ["maça", "morango", "laranja", "uva", "manga", "goiaba"]
 
 # Lista De números 
 
@@ -59,3 +59,73 @@ print(frutas)
 
 print("-" * 80)
 
+#4) ACESSANDO VALORES, INFORMANDO A RESPECTIVA POSIÇÃO
+print("Eleento da QUINTA posição:", frutas[4])
+print("Eleento da PRIMEIRA posição:", frutas[0])
+print("Eleento da ULTIMA posição:", frutas[-1])
+print("Eleento da PENULTIMA posição:", frutas[-2])
+
+#5) SUBSTITUINDO VALORES EXISTENTES
+print("ANTES:", frutas)
+
+#Substituindo o valor da posição 3 (QUARTA posição)
+frutas[3] = "Framboesa"
+#Substituindo o valor da posição 0 (PRIMEIRA posição)
+frutas[0] = "Pitanga"
+#Substituindo o valor da posição -1 (ULTIMA posição)
+frutas[-1] = "Melão"
+
+print("DEPOIS:", frutas)
+
+#6) DETERMINANDO A QUANTIDADE DE ELEMENTOS DA LISTA: len()
+print("Quantidade de elementos da lista de frutas:", len(frutas))
+print("Qantidade de elementos da lista de números:", len(nums))
+
+print("-" * 80 )
+
+#7) REMOVENDO O *ULTIMO* O ULTIMO ELEMENTO DE UMA LISTA: pop() (sem parametrro)
+print("ANTES:", frutas)
+removido = frutas.pop()
+print("Valor removido:", removido)
+print("DEPOS:", frutas)
+
+print("-" * 80 )
+
+#8) REMOVENDO UM ELEMENTO POR SUA POSIÇÃO: pop() (COM parametro)
+print("Removendo elemento da posição 3...")
+removido = frutas.pop(3)
+print(frutas)
+
+print("-" * 80 )
+
+
+#9) REMOVENDO UM ELEMENTO POR SEU VALOR: remove()
+print("Removendo 'uva'...")
+frutas.remove("uva")
+print(frutas)
+
+
+#10) AUMENTANDO UMA LISTA COM ELEMENTOS DE OUTRA LISTA: extend()
+maisFrutas = ['carambola', 'pera', 'acerola', 'jabuticaba', 'caqui']
+frutas.extend(maisFrutas)
+print("Adicionando mais frutas álista... ")
+print(frutas)
+
+#11) FATIANDO UMA LISTA
+#  Fatiar significa copiar um pedaço de uma lista (sublista)
+# criando uma nova lista
+
+#Criar uma nova lista com os elementos das posições de 2 a 5 (posição # 6 *NÃO ENTRA*) da çista de frutas
+subLista2a5 =  frutas[2:6]
+print("Sublista com as frutas das posições de 2 a 5:")
+print(subLista2a5)
+
+# Crie uma sublista que contém os elementos desde o inicio atẃ a posição # 6 (posição 7 *NÃO ENTRE*) 
+subListaAte6 = frutas[7]
+print("Sublista do inicio até a posição 6:")
+print(subListaAte6)
+
+#Crie um sublista qie contém s elementos desde a posição 5 até o final
+sublistaDesde5 = frutas[5]
+print("Sublista da posição 5 até o final:")
+print(sublistaDesde5)
---
+++ 03_range.py
@@ -0,0 +1,33 @@
+"""
+range() é uma função da linguagem Python que gera uma sequência (fixa) de números> É muito usada em associação a lista e á instrução for.
+"""
+
+#1) range() com *UM*pâmetro
+#   Gera uma sequência numérica que vai de 0 (zero) até  valor do parâmetro -1
+for num in range(10):
+    print(num)
+
+print('-' * 80)
+
+#2) range() com *DOIS* parâmetros
+#   1° parâmetro: valor inicial da sequência
+#   2° parâmetro: valor final da seqência (*NÃO INCLUIDO*)
+for x  in range(10,17):
+    print(x)
+
+print('-' * 80)
+
+#3) range() com *TRÊS* parâmetros
+#   1° parâmetro: valor inicial
+#   2° parametro: valor final (*NÃO INCLUIDO*)
+#   3° parâmetro: valor do passo (intervalo entre um número e o seguinte)
+
+for n in range(0,22,3):
+    print(n)
+
+print('-' * 80)
+
+
+# range() com passo negativo (contagem regressiva)
+for i in range (10,0, -1):
+    print(i)
